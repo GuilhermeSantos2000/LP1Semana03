0493e43c96323505ca42daaf341d5a2238c67dde
GuilhermeSantos2000 <guilhermesantos.gamedev@gmail.com>
2025-03-05 21:06:46 +0000

    mudar string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Type a string: ");
+            string phrase = Console.ReadLine();
+
+            Console.Write("Type a letter to be replaced: ");
+            string letter = Console.ReadLine();
+            char SingleLetter = char.Parse(letter);
+
+            foreach (char i in phrase)
+            {
+                if (i == SingleLetter)
+                {
+                Console.Write("x");
+                }
+                else 
+                {
+                Console.Write(i);
+                }
+            }            
+        }
+    }
+}
